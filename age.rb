e2305ab9d95e23cbda6a41c1229f25299b74a3a2
namdoiz <nnamdi.on7@gmail.com>
2021-12-29 17:19:06 -0600

    Variables Exercise

---
+++ age.rb
@@ -0,0 +1,11 @@
+puts "how old are you"
+age = gets.chomp.to_i
+puts "In 10 years you will be #{age + 10}"
+puts "In 20 years you will be #{age + 20}"
+puts "In 30 years you will be #{age + 30}"
+puts "In 40 years you will be #{age + 40}"
+puts "whats your first name"
+first_name = gets.chomp
+puts "whats your last name"
+last_name = gets.chomp
+puts "Hello #{first_name} #{last_name}"
